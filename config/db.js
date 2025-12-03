ab2bb357dac5936f277cf09dfe158ce0dbf83e50
Rebecca John <rebecca.003.john@gmail.com>
2025-12-02 21:43:16 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
 // config/db.js
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const url = process.env.MYSQL_URL;
+if (!url) {
+  throw new Error("MYSQL_URL missing. Set it in Railway -> SkinDEWProject -> Variables.");
+}
+
+const pool = mysql.createPool(url);
 
 module.exports = pool;
+
