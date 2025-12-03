b66bc2d298b8ae5fa3b6d534951991e5dbe0c7ab
John03Rebecca <rebecca.003.john@gmail.com>
2025-12-02 20:37:49 -0500

    Rename folders

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get("/api/admin/session", (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
 --
--- Host: localhost    Database: estore_db
+--  Database: estore_db
 -- ------------------------------------------------------
 -- Server version	8.0.44
 
